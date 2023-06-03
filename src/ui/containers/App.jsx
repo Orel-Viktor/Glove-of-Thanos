ff4fa4d3c41d8d0621e949bf157a102f70efeec3
Orel-Viktor <OrelViktor93@gmail.com>
2023-06-03 12:37:53 +0300

    create logic

--- src/ui/containers/App.jsx
+++ src/ui/containers/App.jsx
@@ -1,5 +1,33 @@
 function App() {
-   console.log(2);
+   let heroes = [
+      'artem',
+      'orel',
+      'jeka',
+      'goft',
+      'gunt',
+      'dimon',
+      'denny',
+      'natasha',
+      'igorek',
+      'kolyan',
+   ];
+   function getRandomInt(max) {
+      return Math.floor(Math.random() * max);
+   }
+   const killHeroes = () => {
+      let luckyHeroes = [];
+      let condition = Math.floor(heroes.length / 2);
+      do {
+         let random = getRandomInt(heroes.length);
+         let luckyHero = heroes[random];
+         luckyHeroes.includes(luckyHero) ? null : luckyHeroes.push(luckyHero);
+      } while (luckyHeroes.length < condition);
+      heroes = luckyHeroes;
+      console.log(heroes);
+   };
+   killHeroes();
+   killHeroes();
+   killHeroes();
 }
 
 export default App;
